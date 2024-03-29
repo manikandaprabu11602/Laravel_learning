c321411e4a80b69b1ef25f1436889ebc3f7659af
manikandaprabu11602 <manikandaprabu157@gmail.com>
2024-03-29 17:49:32 +0530

    backend theme

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
-import './bootstrap';
+import {createApp} from 'vue'
+
+import App from './app.vue'
+
+import '/resources/assets/css/styles.css';
+import '/resources/assets/libs/jquery/dist/jquery.min.js';
+import '/resources/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js';
+import '/resources/assets/js/sidebarmenu.js';
+import '/resources/assets/libs/apexcharts/dist/apexcharts.min.js';
+import '/resources/assets/libs/simplebar/dist/simplebar.js';
+import '/resources/assets/js/dashboard.js';
+
+
+createApp(App).mount("#app")
---
+++ resources/js/app.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="page-wrapper"
+    id="main-wrapper"
+    data-layout="vertical"
+    data-navbarbg="skin6"
+    data-sidebartype="full"
+    data-sidebar-position="fixed"
+    data-header-position="fixed"
+  >
+    <SidebarChildComponent />
+    <div class="body-wrapper">
+      <HeaderChildComponent />
+      <div class="container-fluid">
+        <MainChildComponent />
+        <FooterChildComponent />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SidebarChildComponent from "./sidebar.vue";
+import HeaderChildComponent from "./header.vue";
+import MainChildComponent from "./main.vue";
+import FooterChildComponent from "./footer.vue";
+
+export default {
+  name: "ParentComponent",
+  components: {
+    SidebarChildComponent,
+    HeaderChildComponent,
+    MainChildComponent,
+    FooterChildComponent,
+  },
+};
+</script>
